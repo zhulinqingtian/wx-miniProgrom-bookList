030835bc2fe59c058da5dfbd1d6ba5dd68dcf488
zhulinqingtian <1399873403@qq.com>
2018-11-28 18:11:46 +0800

    Add: 购买页面

---
+++ miniprogram/pages/buy/buy.js
@@ -0,0 +1,81 @@
+var app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    HarvestAddress: '' // 收获地址，默认显示默认地址，没有默认地址时，显示第一个地址
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getHarvestAddress();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+
+  /**
+   * 获取收货地址
+   */
+  getHarvestAddress() {
+    var _this = this,
+      list = app.globalData.addressList,
+      item = {};
+
+    if (!list.length) {
+      return;
+    }
+
+    list.forEach(function (v) {
+      if (v.isDefault) {
+        // 有默认地址
+        item = v;
+      }
+    });
+
+    // 没有默认地址
+    if (JSON.stringify(item) == '{}') {
+      item = list[0];
+    }
+
+    _this.setData({ HarvestAddress: item.province + '-' + item.city + '-' + item.area + ' ' + item.address_details });
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
